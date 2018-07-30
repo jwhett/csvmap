485eb6fd3bcd5f56133de5d1f314e21fd2f3b7b2
Joshua Whetton <whetton.josh@gmail.com>
2018-07-30 00:21:30 -0400

    Added examples for godoc.

---
+++ examples_test.go
@@ -0,0 +1,44 @@
+package csvmap_test
+
+func ExampleNewCsvMap_file() {
+    f, _ := os.Open("test.csv")
+    cm, _ := csvmap.NewCsvMap(f)
+}
+
+func ExampleAddRow() {
+    for _, row := cm.GetRows() {
+        fmt.Println("Row:" row)
+        // Output: "Row: [John Doe jdoe]
+    }
+
+    newRow := make([]string, 3)
+    newRow[0] = "Jane"
+    newRow[1] = "Doe"
+    newRow[2] = "jdoe2"
+
+    cm.AddRow(newRow)
+
+    for _, row := cm.GetRows() {
+        fmt.Println("Row:" row)
+        // Output: "Row: [John Doe jdoe]
+        // Output: "Row: [Jane Doe jdoe2]
+    }
+}
+
+func ExampleGetHeaders() {
+    headers := cm.GetHeaders()
+    for header, _ :range headers {
+        fmt.Println("Header:", header)
+    }
+    // Output: "Header: firstname"
+    // Output: "Header: lastname"
+    // Output: "Header: id"
+}
+
+func ExampleGetRows() {
+    rows := cm.GetRows()
+    for _, row := range rows {
+        fmt.Println("Row:", row)
+    }
+    // Output: "Row: [John Doe jdoe]"
+}
